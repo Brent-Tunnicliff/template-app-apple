6b7d983f5fb39cc2837110e9fe9c5ab5ec5f9018
Brent Tunnicliff <brent@tunnicliff.dev>
2025-09-06 23:46:20 +1000

    Align with recent changes to the lib template.

---
+++ WatchApp/WatchApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
+
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchAppTests/WatchAppTests.swift
@@ -0,0 +1,12 @@
+// Copyright © 2025 Brent Tunnicliff <brent@tunnicliff.dev>
+
+import Testing
+@testable import WatchApp
+
+struct WatchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
